1c81828672fd0475c75f1009f44da442a1441690
rahultechles007 <binodkumarpradhan145@gmail.com>
2026-06-19 21:32:51 +0530

    hello

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        gold: "#C8A96B",
-        beige: "#F5F2EB",
-        cream: "#EEE8DD",
-        premium: "#1E1A17",
-        paragraph: "#6B7280",
-      }
+  gold: "#C8A96B",
+  goldLight: "#E3C98F",
+
+  beige: "#F5F2EB",
+  cream: "#EEE8DD",
+  surface: "#F8F6F1",
+
+  premium: "#1E1A17",
+  accent: "#2A2522",
+  warm: "#3B2F2A",
+
+  paragraph: "#6B7280",
+
+  danger: "#E45858",
+  success: "#2FBF71",
+}
     },
   },
   plugins: [],
